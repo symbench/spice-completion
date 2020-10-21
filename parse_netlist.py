1490b4485211d1e25ef033ddd1aea86d161d786c
Brian Broll <brian.broll@gmail.com>
2020-10-21 15:40:01 -0500

    Don't include pins in graph

--- parse_netlist.py
+++ parse_netlist.py
@@ -2,7 +2,7 @@ import numpy as np
 import networkx as nx
 from PySpice.Spice.Parser import SpiceParser
 from PySpice.Spice import BasicElement
-from PySpice.Spice.Netlist import Pin
+from PySpice.Spice.Netlist import Node
 
 component_types = [
     BasicElement.Resistor,
@@ -10,7 +10,7 @@ component_types = [
     BasicElement.VoltageSource,
     BasicElement.Mosfet,
     BasicElement.SubCircuitElement,
-    Pin,
+    Node,
 ]
 
 def load_netlist(textfile):
@@ -23,21 +23,24 @@ def load_netlist(textfile):
     for element in circuit.elements:
         if element not in component_list:
             component_list.append(element)
-        for pin in element.pins:
-            if pin not in component_list:
-                component_list.append(pin)
+
+        # TODO: ignore the pins
+        nodes = [ pin.node for pin in element.pins ]
+        for node in nodes:
+            if node not in component_list:
+                component_list.append(node)
 
         element_id = component_list.index(element)
         if element_id not in adj:
             adj[element_id] = []
 
-        pin_ids = [component_list.index(pin) for pin in element.pins]
-        adj[element_id].extend(pin_ids)
+        node_ids = [component_list.index(node) for node in nodes]
+        adj[element_id].extend(node_ids)
 
-        for pin_id in pin_ids:
-            if pin_id not in adj:
-                adj[pin_id] = []
-            adj[pin_id].append(element_id)
+        for node_id in node_ids:
+            if node_id not in adj:
+                adj[node_id] = []
+            adj[node_id].append(element_id)
 
     adj = nx.adjacency_matrix(nx.from_dict_of_lists(adj))
 
@@ -50,7 +53,7 @@ def load_netlist(textfile):
     # For now, just mask out the first non-pin (second for validation)
     # TODO: Make the mask more intentional
     mask = np.ones(element_types.size)
-    nonPinIndices = [i for (i, element) in enumerate(component_list) if type(element) is not Pin]
+    nonPinIndices = [i for (i, element) in enumerate(component_list) if type(element) is not Node]
     mask_index = nonPinIndices[0]
     mask[mask_index] = 0
     train_mask = np.array(mask, dtype=np.bool)
