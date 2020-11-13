b5b6c83d2656cd5420d0d74390d2a2866c7707a7
Brian Broll <brian.broll@gmail.com>
2020-11-13 09:54:44 -0600

    Add script for training graph classifier

--- parse_netlist.py
+++ parse_netlist.py
@@ -42,16 +42,16 @@ def get_component_type_index(element):
 
     return component_types.index(element_type)
 
-def load_netlist(textfile):
+def load_masked_netlist(textfile):
     component_list, adj = netlist_as_graph(textfile)
 
     X = np.zeros((len(component_list), len(component_list), len(component_types)))
     A = np.zeros((X.shape[0], adj.shape[0], adj.shape[1]))
     y = np.copy(X)
-    encode_netlist((component_list, adj), A, X, y)
+    encode_masked_netlist((component_list, adj), A, X, y)
     return A, X, y
 
-def load_netlists(textfiles):
+def load_masked_netlists(textfiles):
     graphs = [ netlist_as_graph(textfile) for textfile in textfiles ]
     counts = [ len(components) for (components, _) in graphs ]
     max_components = max(counts)
@@ -64,12 +64,12 @@ def load_netlists(textfiles):
     for (i, (components, adj)) in enumerate(graphs):
         data_points = len(components)
         end = start + data_points
-        encode_netlist((components, adj), A[start:end], X[start:end], y[start:end])
+        encode_masked_netlist((components, adj), A[start:end], X[start:end], y[start:end])
         start = end
 
     return A, X, y
 
-def encode_netlist(graph, A, X, y):
+def encode_masked_netlist(graph, A, X, y):
     (component_list, adj) = graph
     element_types = np.array([ get_component_type_index(e) for e in component_list ])
 
@@ -83,6 +83,40 @@ def encode_netlist(graph, A, X, y):
 
     return A, X
 
+def load_omitted_netlists(textfiles):
+    graphs = [ netlist_as_graph(textfile) for textfile in textfiles ]
+    counts = [ len(components) for (components, _) in graphs ]
+    max_components = max(counts)
+    data_count = sum(counts)
+    A = np.zeros((data_count, max_components, max_components))
+    X = np.zeros((data_count, max_components, len(component_types)))
+    y = np.zeros((data_count, len(component_types)))
+
+    start = 0
+    for (i, (components, adj)) in enumerate(graphs):
+        data_points = len(components)
+        end = start + data_points
+        encode_omitted_netlist((components, adj), A[start:end], X[start:end], y[start:end])
+        start = end
+
+    return A, X, y
+
+def encode_omitted_netlist(graph, A, X, y):
+    (component_list, adj) = graph
+    element_types = np.array([ get_component_type_index(e) for e in component_list ])
+
+    X[:,np.arange(element_types.size), element_types] = 1
+    y[np.arange(element_types.size),element_types] = 1
+    for idx in range(element_types.size):
+        actual_type = element_types[idx]
+        # clear the node representation
+        X[idx, idx, actual_type] = 0
+        # disconnect the node
+        A[idx,:adj.shape[0],:adj.shape[1]] = adj
+        A[idx,:,idx] = 0
+        A[idx,idx,:] = 0
+
+    return A, X
 
 def netlist_as_graph(textfile):
     parser = SpiceParser(source=textfile)
@@ -124,14 +158,65 @@ def is_valid_netlist(textfile, name=None):
             print(f'invalid spice file: {name}', file=sys.stderr)
         return False
 
-# TODO: Load an entire dataset
-# TODO: get the max number of components
+# TODO: Create a dataset where we select where to place the netlist
+# TODO: Create a graph with "candidate" nodes added
+# TODO: Keep the node with the highest likelihood (ie, softmax over the remaining nodes)
+def load_placement_netlists(textfiles):
+    graphs = [ netlist_as_graph(textfile) for textfile in textfiles ]
+    counts = [ len(components) for (components, _) in graphs ]
+    print([ components for (components, _) in graphs ])
+    # TODO: account for the added nodes
+    # TODO: elements can only be connected to nodes...
+
+    # TODO: add new elements to the graph
+    max_components = max(counts)
+    max_components = max_components + (max_components * (max_components - 1))
+    data_count = sum(counts)
+    A = np.zeros((data_count, max_components, max_components))
+    X = np.zeros((data_count, max_components, len(component_types)))
+    y = np.zeros((data_count, max_components))
+
+    start = 0
+    for (i, (components, adj)) in enumerate(graphs):
+        data_points = len(components)
+        end = start + data_points
+        encode_placement_netlist((components, adj), A[start:end], X[start:end], y[start:end])
+        start = end
+
+    return A, X, y
+
+def encode_placement_netlist(graph, A, X, y):
+    (component_list, adj) = graph
+    element_types = np.array([ get_component_type_index(e) for e in component_list ])
+
+    print(adj)
+    print(adj.shape)
+    node_idx = [ i for (i, c) in enumerate(component_list) if type(c) is Node ]
+    all_indices = (v for v in np.stack(np.meshgrid(node_idx, node_idx), -1).reshape(-1, 2))
+    indices = [(src, dst) for (src, dst) in all_indices if src < dst ]
+    # TODO: update adjacency
+    # TODO: update components?
+    print(indices, len(indices))
+    exit_;
+
+    X[:,np.arange(element_types.size), element_types] = 1
+    y[:,np.arange(element_types.size), range(element_types.size)] = 1
+    for idx in range(element_types.size):
+        actual_type = element_types[idx]
+        X[idx, idx, actual_type] = 0
+        X[idx, idx, 0] = 1
+        A[idx,:adj.shape[0],:adj.shape[1]] = adj
+        print(idx)
+        # TODO: Add the extra nodes
+
+    return A, X
 
 if __name__ == '__main__':
     import sys
     if len(sys.argv) > 1:
         with open(sys.argv[1], 'rb') as f:
-            print(load_netlist(f.read().decode('utf-8', 'ignore')))
+            #print(load_masked_netlist(f.read().decode('utf-8', 'ignore')))
+            print(load_placement_netlists([f.read().decode('utf-8', 'ignore')]))
     else:
         import json
         types = {}
--- train_graph_classifier.py
+++ train_graph_classifier.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-from spektral.layers import GraphAttention
+from spektral.layers import GraphAttention, GlobalAttentionPool
 
 """
 This example reuses the Graph Attn Network from the paper:
@@ -10,15 +10,12 @@ Petar Veličković, Guillem Cucurull, Arantxa Casanova, Adriana Romero, Pietro L
 """
 
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import Input, Dropout
+from tensorflow.keras.layers import Input, Dense, Flatten
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.regularizers import l2
 
-from spektral.datasets import citation
-from spektral.layers import GraphAttention
-
-from parse_netlist import load_masked_netlists, is_valid_netlist
+from parse_netlist import load_omitted_netlists, is_valid_netlist
 import sys
 import argparse
 
@@ -42,7 +39,7 @@ def sample(A, X, y, amt, indices=None):
 # Load data
 netlists = ( open(f, 'rb').read().decode('utf-8', 'ignore') for f in args.files )
 netlists = ( text for text in netlists if is_valid_netlist(text) )
-A, X, y = load_masked_netlists(netlists)
+A, X, y = load_omitted_netlists(netlists)
 
 train_A, train_X, train_y, indices = sample(A, X, y, 0.8)
 val_A, val_X, val_y, indices = sample(A, X, y, 0.5, indices)
@@ -55,9 +52,7 @@ n_attn_heads = 8        # Number of attention heads in first GAT layer
 N = train_X.shape[0]          # Number of nodes in the graph
 F = train_X.shape[-1]          # Original size of node features
 n_classes = train_y.shape[-1]  # Number of classes
-dropout = 0.6           # Dropout rate for the features and adjacency matrix
-dropout = 0.  # FIXME: remove
-l2_reg = 5e-6           # L2 regularization rate
+l2_reg = 5e-4           # L2 regularization rate
 learning_rate = 5e-3    # Learning rate
 epochs = args.epochs
 es_patience = 100       # Patience for early stopping
@@ -67,30 +62,17 @@ A = A.astype('f4')
 #X = X.toarray()
 
 # Model definition
-X_in = Input(shape=(F, ))
-A_in = Input(shape=(N, ))
+X_in = Input(shape=(F,))
+A_in = Input(shape=(N,))
 
-dropout_1 = Dropout(dropout)(X_in)
-graph_attention_1 = GraphAttention(channels,
-                                   attn_heads=n_attn_heads,
-                                   concat_heads=True,
-                                   dropout_rate=dropout,
-                                   activation='elu',
-                                   kernel_regularizer=l2(l2_reg),
-                                   attn_kernel_regularizer=l2(l2_reg),
-                                   )([dropout_1, A_in])
-dropout_2 = Dropout(dropout)(graph_attention_1)
-graph_attention_2 = GraphAttention(n_classes,
-                                   attn_heads=1,
-                                   concat_heads=False,
-                                   dropout_rate=dropout,
-                                   activation='softmax',
-                                   kernel_regularizer=l2(l2_reg),
-                                   attn_kernel_regularizer=l2(l2_reg),
-                                   )([dropout_2, A_in])
+gc1 = GraphAttention(32, activation='relu', kernel_regularizer=l2(l2_reg))([X_in, A_in])
+gc2 = GraphAttention(32, activation='relu', kernel_regularizer=l2(l2_reg))([gc1, A_in])
+pool = GlobalAttentionPool(128)(gc2)
+
+output = Dense(n_classes, activation='softmax')(Flatten()(pool))
 
 # Build model
-model = Model(inputs=[X_in, A_in], outputs=graph_attention_2)
+model = Model(inputs=[X_in, A_in], outputs=output)
 optimizer = Adam(lr=learning_rate)
 model.compile(optimizer=optimizer,
               loss='categorical_crossentropy',
@@ -99,11 +81,14 @@ model.summary()
 
 # Train model
 unknowns = train_X.argmin(2)
+print(train_X.shape)
+print(train_A.shape)
+print(train_y.shape)
 print(f'about to train on {train_X.shape[0]} points ({X.shape[0]} total)')
 validation_data = ([val_X, val_A], val_y)
 history = model.fit([train_X, train_A],
           train_y,
-          sample_weight=unknowns,
+          #sample_weight=unknowns,
           epochs=epochs,
           #batch_size=N,
           validation_data=validation_data,
@@ -132,12 +117,14 @@ import scikitplot as skplt
 def plot_confusion_matrix(A, X, y):
     plt.clf()
     y_pred = model.predict([X, A])
-    pred_unk = X.argmin(2)*y_pred.argmax(2)
-    act_unk = X.argmin(2)*y.argmax(2)
-    y_pred = pred_unk[pred_unk.nonzero()]
-    y_act = act_unk[act_unk.nonzero()]
-    print('  actual unknowns:\t', y_act)
-    print('  predicted unknowns:\t', y_pred)
+    #pred_unk = X.argmin(2)*y_pred.argmax(2)
+    #act_unk = X.argmin(2)*y.argmax(2)
+    #y_pred = pred_unk[pred_unk.nonzero()]
+    #y_act = act_unk[act_unk.nonzero()]
+    y_pred = y_pred.argmax(1)
+    y_act = y.argmax(1)
+    print('  predicted:\t', y_pred)
+    print('  actual:\t', y_act)
     skplt.metrics.plot_confusion_matrix(y_act, y_pred)
 
 print('Generating confusion matrices...')
